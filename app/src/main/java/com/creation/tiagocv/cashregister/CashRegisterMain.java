c0d61092ca108727e2c2a0a143697100c0125509
TIAGOCV-PC\tcver <tcverdade.64@gmail.com>
2017-03-09 19:36:29 +0000

    The Beginning

---
+++ app/src/main/java/com/creation/tiagocv/cashregister/CashRegisterMain.java
@@ -0,0 +1,13 @@
+package com.creation.tiagocv.cashregister;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CashRegisterMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cash_register_main);
+    }
+}
